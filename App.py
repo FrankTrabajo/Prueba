ef44068d9fab2f5b6d6b923d18c6d828f6405e8f
at15DAW1 <at15DAW1@informatica.iesjc>
2024-05-22 20:00:54 +0200

    comienzo calculadora

--- App.py
+++ App.py
@@ -1,5 +1,24 @@
 import tkinter as tk
+from tkinter import *
 
-class Ventana():
+class Ventana:
 
-    def __init__(self):
+    def __init__(self, root):
+        self.ventana = root
+        self.ventana.title("Calculadora")
+        self.ventana.resizable(1,1)
+        
+        #Creacion del contenedor del Frame principal
+        frame = LabelFrame(self.ventana, text="Calculadora",font=('Calibri', 16, 'bold'))
+        frame.grid(row=0,column=0,columnspan=4,padx=20, pady=20)
+        #Label donde se verá el resultado por pantalla
+        self.ventanaNumerica = Label(frame)
+        self.ventanaNumerica.grid(row=0,column=0, sticky=N + E)
+        #Botones de numeros
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    app = Ventana(root)
+    root.mainloop()
